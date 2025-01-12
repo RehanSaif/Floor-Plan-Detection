d6eea26c652b12b1386317b17184676b4c190591
RehanSaif <32071200+RehanSaif@users.noreply.github.com>
2025-01-12 20:51:21 +0000

    changes

--- utils/plotting.py
+++ utils/plotting.py
@@ -588,13 +588,13 @@ def discrete_cmap():
              '#fccde5', '#80b1d3', '#808080', '#fb8072', '#696969',
              '#577a4d', '#ffffb3']
     cmap3 = colors.ListedColormap(cpool, 'rooms')
-    cm.register_cmap(cmap=cmap3)
+    plt.colormaps.register(cmap=cmap3)
 
     cpool = ['#DCDCDC', '#8dd3c7', '#b15928', '#fdb462', '#ffff99',
              '#fccde5', '#80b1d3', '#808080', '#fb8072', '#696969',
              '#577a4d']
     cmap3 = colors.ListedColormap(cpool, 'icons')
-    plt.register_cmap(cmap=cmap3)
+    plt.colormaps.register(cmap=cmap3)
 
     """create a colormap with N (N<15) discrete colors and register it"""
     # define individual colors as hex values
@@ -602,13 +602,13 @@ def discrete_cmap():
              '#fccde5', '#80b1d3', '#808080', '#fb8072', '#696969',
              '#577a4d', '#ffffb3', '#d3d5d7']
     cmap3 = colors.ListedColormap(cpool, 'rooms_furu')
-    cm.register_cmap(cmap=cmap3)
+    plt.colormaps.register(cmap=cmap3)
 
     cpool = ['#DCDCDC', '#8dd3c7', '#b15928', '#fdb462', '#ffff99',
              '#fccde5', '#80b1d3', '#808080', '#fb8072', '#696969',
              '#577a4d']
     cmap3 = colors.ListedColormap(cpool, 'icons_rooms_furu')
-    cm.register_cmap(cmap=cmap3)
+    plt.colormaps.register(cmap=cmap3)
 
 
 def segmentation_plot(rooms_pred, icons_pred, rooms_label, icons_label):
